5c0d975ea73ac62079b0d86b9a307f95e465f428
Ebenezer Tseh <ebenezertseh2002@gmail.com>
2024-06-01 23:24:12 -0400

    revised topk

--- Arrays and Hashing/topkfreq_e.py
+++ Arrays and Hashing/topkfreq_e.py
@@ -1,26 +1,32 @@
 def topKFrequent(nums, k):
-    # using backet sort, create a two d array 
+    # first count to  get the frequency of the nums
+    # sort the elements in the hashmap into a 2d array using bucket sort, to store the elements 
+    # with the bucket sort having the frequency of the numbers, we can first k elements in a descending order 
 
-    # use hashmap to count elements
+
+    # count
+
     count = {}
-    freq =[[] for _ in range(len(nums)+ 1) ]
+
     for num in nums:
-        count[num] = 1 + count.get(num, 0)
-    
-    for n, c in count.items():
-        freq[c].append(n)
+        count[num] = 1 + count.get(num, 0) 
 
+    # create a frequency two d array, append elemnts at the frequency. in O(n)
+
+    freq = [[] for _ in range(len(nums) + 1)]
+
+    # itereate through the hashmap geting key value pairs, 
+
+    for num, f in count.items():
+        freq[f].append(num)
     res = []
-
     for i in range(len(freq) - 1, 0, -1):
         for n in freq[i]:
-            res.append(n) 
+            res.append(n)
             if len(res) == k:
                 return res
 
-    
 
-    
 
 
 
