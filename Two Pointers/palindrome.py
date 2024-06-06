9c26fd87183d06175f2bbd186769a47058adc7c4
Ebenezer Tseh <ebenezertseh2002@gmail.com>
2024-06-06 17:29:34 -0400

    Two pointers day2

--- Two Pointers/palindrome.py
+++ Two Pointers/palindrome.py
@@ -1,29 +1,29 @@
-def ispalindrome(s):
-        l_ptr = 0
-        r_ptr = len(s) - 1
+# def ispalindrome(s):
+#         l_ptr = 0
+#         r_ptr = len(s) - 1
         
-        def alphaNum(c):
+#         def alphaNum(c):
 
-            return (ord('A') <= ord(c) <= ord('Z') or 
-            ord('a') <= ord(c) <= ord('z') or 
-            ord('0') <= ord(c) <= ord('9') )
+#             return (ord('A') <= ord(c) <= ord('Z') or 
+#             ord('a') <= ord(c) <= ord('z') or 
+#             ord('0') <= ord(c) <= ord('9') )
      
-        while l_ptr < r_ptr:
-            while l_ptr < r_ptr and not alphaNum(s[l_ptr]):
-                l_ptr += 1
-            while r_ptr > l_ptr and not alphaNum(s[r_ptr]):
-                r_ptr -= 1
+#         while l_ptr < r_ptr:
+#             while l_ptr < r_ptr and not alphaNum(s[l_ptr]):
+#                 l_ptr += 1
+#             while r_ptr > l_ptr and not alphaNum(s[r_ptr]):
+#                 r_ptr -= 1
 
 
-            if s[l_ptr].lower() != s[r_ptr].lower():
-                return False
-            l_ptr += 1
-            r_ptr -= 1
+#             if s[l_ptr].lower() != s[r_ptr].lower():
+#                 return False
+#             l_ptr += 1
+#             r_ptr -= 1
             
             
 
             
-        return True
+#         return True
             
             
             
@@ -48,4 +48,6 @@ def ispalindrome(s):
 
 
 
-print(ispalindrome("aba"))
+# print(ispalindrome("aba"))
+
+print("12a".isalnum())
