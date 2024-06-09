2e173bf982c2c44e83bf2b8e69796f7d13f57277
Ebenezer Tseh <ebenezertseh2002@gmail.com>
2024-06-09 15:37:42 -0400

    min stack implementation

--- codepath/prac.py
+++ codepath/prac.py
@@ -4,7 +4,6 @@ def __init__(self):
         
         
         
-
 def ping(self, t: int) -> int:
 
         self.queue.append(t)
@@ -14,3 +13,27 @@ def ping(self, t: int) -> int:
 
         return len(self.queue)
        
+
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.minStack.append(min(val, self.minStack[-1] if self.minStack else val))
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+      
+        
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
