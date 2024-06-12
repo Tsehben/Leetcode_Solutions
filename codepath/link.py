d80385c777ac81d4ec5f9ad846d121bb2d927473
Ebenezer Tseh <ebenezertseh2002@gmail.com>
2024-06-12 13:56:22 -0400

    Linked Llist

--- codepath/link.py
+++ codepath/link.py
@@ -1,4 +1,36 @@
 class Node:
-    def __init__(self):
+    def __init__(self, data = None, next= None):
         self.data = data 
-        self.next = 
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def print_list(self):
+        if self.head is None:
+            print("Linked List is empty")
+            return 
+        
+        itr = self.head
+        llitr = ""
+        while itr:
+            llitr += str(itr.data) + ("-->" if itr.next else "")
+            itr = itr.next
+
+        print(llitr)
+        return
+    
+
+ll = LinkedList()
+
+ll.insert_at_beginning(3)
+ll.insert_at_beginning(10)
+ll.insert_at_beginning(9)
+ll.insert_at_beginning(7)
+ll.insert_at_beginning(6)
+ll.print_list()
