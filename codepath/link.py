f78fafad06dc9c9473533223b034ad5a0de73132
Ebenezer Tseh <ebenezertseh2002@gmail.com>
2024-06-13 00:47:12 -0400

    ll

--- codepath/link.py
+++ codepath/link.py
@@ -25,6 +25,65 @@ class LinkedList:
         print(llitr)
         return
     
+    def insert_at_end(self, data):
+        if self.head is None:
+            node = Node(data, None)
+            self.head = node
+            return
+        
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+
+        node = Node(data, None)
+        itr.next = node
+        return 
+    
+    def insert_values(self, data_list):
+        self.head = None
+
+        for data in data_list:
+            self.insert_at_end(data)
+        
+
+    def get_length(self):
+        count = 0
+
+        if self.head is None:
+            print(count)
+            return 
+        
+        count = 1
+        itr = self.head
+        while itr.next:
+            count += 1
+            itr = itr.next
+        
+
+        return count
+        
+    def remove(self, index):
+        if index < 0 or index > self.get_length():
+            raise Exception("Invalid index")
+        if index == 0:
+            self.head = self.head.next
+            return
+        
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count += 1
+
+        return
+    
+        
+
+
+    
 
 ll = LinkedList()
 
@@ -33,4 +92,12 @@ ll.insert_at_beginning(10)
 ll.insert_at_beginning(9)
 ll.insert_at_beginning(7)
 ll.insert_at_beginning(6)
+ll.insert_at_end(89)
+ll.insert_at_end(80)
+# ll.insert_values(["Eben","Kwaku", "Tseh"])
+
+
+#ll.get_length()
+# ll.remove(2)
+ll.remove(4)
 ll.print_list()
