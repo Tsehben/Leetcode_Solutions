639f143e4bcd7dfcf1b5c49724666632409c89f8
Ebenezer Tseh <ebenezertseh2002@gmail.com>
2024-06-17 23:03:13 -0400

    palindrome linkedlist

--- codepath/link3.py
+++ codepath/link3.py
@@ -50,4 +50,42 @@ class Solution:
              cur = nxt
         
         return prev
+    
+
+
+# class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # find mid point
+
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # slow becomes the mid- point
+
+        # reverse the linked list from the midpoint going to the end
+
+        prev = None
+
+        
+
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+
+        # check for palindrome
+
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+
+        return True
+
         
